d45bab85d3ce84905c2fa3e61afdfa359e4e18d5
Hector Gatica <hectorgatica99@gmail.com>
2022-09-19 17:17:37 -0400

    Added more entries

--- HW 1&2/SQLQuery5.sql
+++ HW 1&2/SQLQuery5.sql
@@ -1,8 +1,41 @@
 INSERT INTO dbo.Taverns 
 (TavernName)
-VALUES ('John''s')
+VALUES ('John''s Tavern')
 
+INSERT INTO dbo.Taverns 
+(TavernName)
+VALUES ('Mikes''s Tavern')
+
+INSERT INTO dbo.Taverns 
+(TavernName)
+VALUES ('Jake''s Tavern')
+
+INSERT INTO dbo.Taverns 
+(TavernName)
+VALUES ('Richie''s Tavern')
+
+INSERT INTO dbo.Taverns 
+(TavernName)
+VALUES ('Greg''s Tavern')
+
+--Seeding Users DB
 INSERT INTO dbo.Users
 (UserName)
 VALUES ('Mike')
+
+INSERT INTO dbo.Users
+(UserName)
+VALUES ('Kim')
+
+INSERT INTO dbo.Users
+(UserName)
+VALUES ('Jim')
+
+INSERT INTO dbo.Users
+(UserName)
+VALUES ('Henry')
+
+INSERT INTO dbo.Users
+(UserName)
+VALUES ('Phil')
 

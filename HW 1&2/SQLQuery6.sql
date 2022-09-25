714e6914345fba7da52878180cff8240053c6af9
Hector Gatica <hectorgatica99@gmail.com>
2022-09-24 20:29:24 -0400

    Update SQLQuery6.sql
    Added additional tables needed for homework 2, and additional fields for SELECT and JOIN functions to work properly for hw 3 and 4

--- HW 1&2/SQLQuery6.sql
+++ HW 1&2/SQLQuery6.sql
@@ -2,89 +2,160 @@
 DROP TABLE IF EXISTS Taverns;
 DROP TABLE IF EXISTS Locations;
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Roles;
+DROP TABLE IF EXISTS Guests;
+DROP TABLE IF EXISTS GuestStatuses;
 DROP TABLE IF EXISTS BasementRats;
 DROP TABLE IF EXISTS Sales;
-DROP TABLE IF EXISTS Inventory;
-DROP TABLE IF EXISTS InventorySales;
 DROP TABLE IF EXISTS Classes;
 DROP TABLE IF EXISTS TavernServices;
+DROP TABLE IF EXISTS Services;
+DROP TABLE IF EXISTS Supplies;
+DROP TABLE IF EXISTS Rooms;
+DROP TABLE IF EXISTS RoomStatuses;
+DROP TABLE IF EXISTS Stays;
+DROP TABLE IF EXISTS Levels;
+DROP TABLE IF EXISTS Received;
+DROP TABLE IF EXISTS Counts;
+DROP TABLE IF EXISTS ServiceStatuses;
 
-
---Creates Taverns Table
 CREATE TABLE Taverns
 (
-	ID INT PRIMARY KEY IDENTITY(1,1),
+	Id INT PRIMARY KEY IDENTITY(1,1),
 	TavernName VARCHAR(128),
-	UserID INT,
-	SalesID INT,
-	InventoryID INT,
-	InventorySalesID INT
+	OwnerId INT,
+	LocationId INT
 );
---Creates Inventory Table
-CREATE TABLE Inventory
+
+CREATE TABLE Users
 (
-	ID INT PRIMARY KEY IDENTITY(1,1),
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	UserName VARCHAR(250),
+	RoleId INT
+);
+
+CREATE TABLE Levels
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	GuestId INT,
+	ClassId INT
+);
+
+CREATE TABLE Guests
+(	
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	GuestName VARCHAR(250),
+	GuestNotes VARCHAR(250),
+	GuestBirthDay DATE,
+	GuestCakeDay DATE,
+	StatusId INT
+
+);
+
+CREATE TABLE GuestStatuses
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	StatusName VARCHAR(250)
+);
+
+CREATE TABLE Roles
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	RoleName INT,
+	RoleDescription VARCHAR(250)
+);
+
+CREATE TABLE Supplies
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
 	TavernId INT FOREIGN KEY (TavernId) REFERENCES Taverns(ID),
 	Item VARCHAR (250),
 	Quantity FLOAT,
 	UnitsOfMeasure VARCHAR (250)
 );
 
---Creates TavernServices Table. This isn't a many-to-many table, just outlining services. A seperate Services table and then a many-to-many table would be better
-CREATE TABLE TavernServices
+CREATE TABLE Received
 (
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	TavernId INT FOREIGN KEY (TavernID) REFERENCES Taverns(ID),
-	TavernService VARCHAR (250),
-	Price FLOAT,
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	SupplyId INT,
+	ReceivedDate DATE,
+	Amount FLOAT,
+	Cost FLOAT,
+	TavernId INT
+
 );
 
---Creates Sales Table
+CREATE TABLE Services
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	TavernId INT,
+	StatusId INT
+);
+
+CREATE TABLE ServiceStatuses
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	StatusName VARCHAR(250)
+);
+
 CREATE TABLE Sales
 ( 
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	TavernId INT FOREIGN KEY (TavernId) REFERENCES Taverns(ID)
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	TavernId INT,
+	GuestId INT,
+	SalesPrice FLOAT,
+	SalesDate DATE,
+	SalesAmount INT
 );
 
---Creates InventorySales Table
-CREATE TABLE InventorySales
+CREATE TABLE Counts
 (
-	ID INT PRIMARY KEY IDENTITY(1,1)
+	SupplyId INT,
+	TavernId INT,
+	CountsDate DATE,
+	Count FLOAT
 );
 
---Creates Location Table
+CREATE TABLE Rooms
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	RoomName VARCHAR(250),
+	TavernId INT,
+	RoomStatusId INT
+);
+
+CREATE TABLE RoomStatuses
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	RoomStatusName VARCHAR(250)
+);
+
+CREATE TABLE Stays
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	StayRate FLOAT,
+	SalesId INT,
+	GuestId INT,
+	RoomId INT,
+	StayDate DATE
+);
+
 CREATE TABLE Locations
 (
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	TavernId INT FOREIGN KEY (TavernID) REFERENCES Taverns(ID),
+	Id INT PRIMARY KEY IDENTITY(1,1),
 	LocationName VARCHAR(250)
 );
 
---Creates Users Table
-CREATE TABLE Users
-(
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	UserName VARCHAR(250),
-	Notes VARCHAR(250),
-	BirthDay DATE,
-	CakeDay DATE,
-	UserStatus VARCHAR(250),
-	TavernId INT
-)
-
---Creates Classes Table
 CREATE TABLE Classes
 (
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	UserClass VARCHAR (250),
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	ClassName VARCHAR (250),
 
 );
 
---CREATES BasementRats Table
 CREATE TABLE BasementRats
 (
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	TavernId INT FOREIGN KEY (TavernId) REFERENCES Taverns(ID),
+	Id INT PRIMARY KEY IDENTITY(1,1),
 	RatName VARCHAR(250)
 );
 

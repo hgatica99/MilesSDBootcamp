3f896cda5f781ba02ebcf9eab710f2e31c4417f2
Hector Gatica <hectorgatica99@gmail.com>
2022-09-27 08:18:17 -0400

    Homework 4 Answers
    Still need to finish

---
+++ Homework Files/SQLQuery2.sql
@@ -0,0 +1,61 @@
+--HW 4
+
+--#1
+SELECT Users.UserName
+FROM Users
+WHERE Users.RoleId = 1
+
+--#2 I could add seeds to the location table, and then add an additional join to pull location name instead of locationId.
+SELECT Users.UserName, Locations.LocationName, Roles.RoleName
+FROM Taverns
+JOIN Users ON Users.Id = Taverns.OwnerId
+JOIN Roles ON Users.RoleId = Roles.Id
+JOIN Locations ON Taverns.LocationId = Locations.Id
+WHERE Users.RoleId = 1
+
+--#3
+SELECT Guests.GuestName, Classes.ClassName, Levels.Level
+FROM Guests
+JOIN Levels ON Guests.Id = Levels.GuestId
+JOIN Classes on Levels.ClassId = Classes.Id
+ORDER BY GuestName ASC
+
+--#4
+SELECT TOP 10 Sales.SalesPrice, Services.Name
+FROM Sales
+JOIN Services ON Sales.ServiceId = Services.Id
+ORDER BY SalesPrice DESC
+
+--#5
+SELECT Guests.Id, Guests.GuestName, Classes.ClassName, Levels.Level
+FROM Guests
+JOIN Levels ON Guests.Id = Levels.GuestId
+JOIN Classes ON Classes.Id = Levels.ClassId
+WHERE Guests.Id IN (SELECT Levels.GuestId 
+					FROM Levels
+					GROUP BY GuestId
+					HAVING COUNT(GuestId) >= 2)
+
+--#6
+SELECT Guests.Id, Guests.GuestName, Classes.ClassName, Levels.Level
+FROM Guests
+JOIN Levels ON Guests.Id = Levels.GuestId
+JOIN Classes ON Classes.Id = Levels.ClassId
+WHERE Guests.Id IN (SELECT Levels.GuestId 
+					FROM Levels
+					GROUP BY GuestId
+					HAVING COUNT(GuestId) >= 1)
+				AND Levels.Level > 5
+
+--#7
+SELECT GuestName, ClassName, Level
+FROM (SELECT Guests.GuestName, Classes.ClassName, Levels.Level, MAX(Level) over (PARTITION BY ))
+
+	
+SELECT Levels.GuestId, MAX(Levels.Level) Level
+FROM Levels
+GROUP BY GuestId
+
+
+SELECT *
+FROM Guests

0c3787f85a7e5358ebb7681f99b6b0cd3d2356cc
Hector Gatica <hectorgatica99@gmail.com>
2022-09-29 13:05:00 -0400

    Fixed Spacing & Naming

--- Homework Files/SQLQuery3.sql
+++ Homework Files/SQLQuery3.sql
@@ -46,7 +46,7 @@ SELECT *
 FROM dbo.FinalTable(28)  
 
 --#5 
-CREATE FUNCTION TableNumber5 (@date DATE)
+CREATE FUNCTION RoomsOpenOnDate (@date DATE)
 RETURNS TABLE
 AS
 RETURN (SELECT Rooms.RoomName, Taverns.TavernName
@@ -56,10 +56,10 @@ RETURN (SELECT Rooms.RoomName, Taverns.TavernName
 		WHERE NOT @date = Stays.StayDate)
 
 SELECT *
-FROM dbo.TableNumber5('02/18/2022')
+FROM dbo.RoomsOpenOnDate('02/18/2022')
 
 --#6
-CREATE FUNCTION TableNumber6 (@min FLOAT, @max FLOAT)
+CREATE FUNCTION RoomsInPriceRange (@min FLOAT, @max FLOAT)
 RETURNS TABLE
 AS
 RETURN (SELECT T.Id TavernId, T.TavernName, R.RoomName, R.Price
@@ -67,35 +67,30 @@ RETURN (SELECT T.Id TavernId, T.TavernName, R.RoomName, R.Price
 		JOIN Rooms R ON R.Id = T.Id
 		WHERE R.Price >= @min AND R.Price <= @max)
 
---#7
+--#7 Original
 INSERT INTO Rooms
 (RoomName, Price, TavernId, RoomStatusId)
-VALUES('New Room', (SELECT MIN(Price) FROM dbo.TableNumber6(10.00, 150.00))-0.01, 1, 1)
+VALUES('New Room', (SELECT MIN(Price) FROM dbo.RoomsInPriceRange(10.00, 150.00))-0.01, 1, 1)
 
---#7 Experimental, adds new room to random tavern that's not the cheapest one.
+--#7 Experimental
 INSERT INTO Rooms
 (RoomName, Price, TavernId, RoomStatusId)
 VALUES('New Room', 
-       (SELECT MIN(Price) 
-	FROM dbo.TableNumber6(10.00, 150.00))-0.01, 
-       	(SELECT TOP 1 T.Id
-	FROM Taverns T
-	WHERE NOT T.Id = (SELECT T2.TavernId 
-			  FROM dbo.TableNumber6(10.00, 150.00) T2 
-			  WHERE T2.Price = (SELECT MIN(Price) 
-					    FROM dbo.TableNumber6(10.00, 150.00)))
- 					    ORDER BY NEWID()), 
-       1)
+	  (SELECT MIN(Price) FROM dbo.RoomsInPriceRange(10.00, 150.00))-0.01, 
+	  (SELECT TOP 1 T.Id FROM Taverns T WHERE NOT T.Id = (SELECT T2.TavernId FROM dbo.RoomsInPriceRange(10.00, 150.00) T2 WHERE T2.Price = (SELECT MIN(Price) FROM dbo.RoomsInPriceRange(10.00, 150.00)))ORDER BY NEWID()),
+	  1)
+
 
 --Returns Random TavernID from the table returned by Function TableNumber6 that doesn't match the tavern id with the matched price. Future issues are: What if there are 2 taverns with the same price? How will id's be matched?																								  --
-SELECT TOP 1 T.Id
-FROM Taverns T
-WHERE NOT T.Id = (SELECT T2.TavernId 
-		  FROM dbo.TableNumber6(10.00, 150.00) T2 
-	  	  WHERE T2.Price = (SELECT MIN(Price) FROM dbo.TableNumber6(10.00, 150.00)))
+SELECT TOP 1 T.TavernId
+FROM dbo.RoomsInPriceRange(10.00, 150.00) T
+WHERE NOT T.TavernId = (SELECT T2.TavernId FROM dbo.RoomsInPriceRange(10.00, 150.00) T2 WHERE T2.Price = (SELECT MIN(Price) FROM dbo.RoomsInPriceRange(10.00, 150.00)))
 ORDER BY NEWID()
+
 
 SELECT *
 FROM Stays
 SELECT *
 FROM Rooms
+SELECT *
+FROM Taverns

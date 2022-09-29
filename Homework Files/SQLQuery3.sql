f92aaaa7690949c7cfc7d6171628fff0ddc9bff0
Hector Gatica <hectorgatica99@gmail.com>
2022-09-29 12:03:19 -0400

    Improved #7

--- Homework Files/SQLQuery3.sql
+++ Homework Files/SQLQuery3.sql
@@ -72,16 +72,16 @@ INSERT INTO Rooms
 (RoomName, Price, TavernId, RoomStatusId)
 VALUES('New Room', (SELECT MIN(Price) FROM dbo.TableNumber6(10.00, 150.00))-0.01, 1, 1)
 
---#7 Experimental
+--#7 Experimental, adds new room to random tavern that's not the cheapest one.
 INSERT INTO Rooms
 (RoomName, Price, TavernId, RoomStatusId)
 VALUES('New Room', (SELECT MIN(Price) 
-					FROM dbo.TableNumber6(10.00, 150.00))-0.01, (SELECT TOP 1 T.TavernId
-																 FROM dbo.TableNumber6(10.00, 150.00) T
-																 WHERE NOT T.TavernId = (SELECT T2.TavernId 
-																						 FROM dbo.TableNumber6(10.00, 150.00) T2 
-																						 WHERE T2.Price = (SELECT MIN(Price) 
-																										   FROM dbo.TableNumber6(10.00, 150.00)))ORDER BY NEWID()), 1)
+					FROM dbo.TableNumber6(10.00, 150.00))-0.01, (SELECT TOP 1 T.Id
+																 FROM Taverns T
+																 WHERE NOT T.Id = (SELECT T2.TavernId 
+																					FROM dbo.TableNumber6(10.00, 150.00) T2 
+																					WHERE T2.Price = (SELECT MIN(Price) 
+																									  FROM dbo.TableNumber6(10.00, 150.00)))ORDER BY NEWID()), 1)
 
 --Returns Random TavernID from the table returned by Function TableNumber6 that doesn't match the tavern id with the matched price. Future issues are: What if there are 2 taverns with the same price? How will id's be matched?																								  --
 SELECT TOP 1 T.TavernId
